b568a1d24b937210f04222acf8f85b7e53034a67
Joseph Butterfield <raleigh9123@gmail.com>
2020-04-01 01:29:49 -0700

    Database Sync and Homepage View
    -Sync database with app in www file. Nest 'server.listen' within sequelize sync function
    -Add asynchronous function handler in books.js
    -Render index page to display all books.
    -Pass book data into template

--- routes/books.js
+++ routes/books.js
@@ -1,21 +1,24 @@
 const express = require('express');
 
 const router = express.Router();
-const Book = require('../models').Article;
+const Book = require('../models').Book;
 
-const asyncHandler = cb => {
-  return async(req,res,next) => {
+function asyncHandler(cb){
+  return async(req, res, next) => {
     try {
       await cb(req, res, next)
-    } catch(error) {
+    } catch(error){
       res.status(500).send(error);
     }
   }
 }
 
 /* GET books list. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll({
+    order: [['createdAt', 'DESC']]
+  });
+  res.render("books/index", { books, title: "Sequelize-It!" });
+}));
 
 module.exports = router;
