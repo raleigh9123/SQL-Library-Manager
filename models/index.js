be8aee2451c309f7e1d6f9a63547a2c7555e09d0
Joseph Butterfield <raleigh9123@gmail.com>
2020-04-23 22:36:50 -0700

    Update code formatting (ESLINT)

--- models/index.js
+++ models/index.js
@@ -1,11 +1,12 @@
-'use strict';
+
 
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+const config = require(`${__dirname  }/../config/config.json`)[env];
 const db = {};
 
 let sequelize;
@@ -21,7 +22,7 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
-    const model = sequelize['import'](path.join(__dirname, file));
+    const model = sequelize.import(path.join(__dirname, file));
     db[model.name] = model;
   });
 
